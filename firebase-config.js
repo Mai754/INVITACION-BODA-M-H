b330df92e386f9d9264c2b7f412d39d07e3dbc0c
Mai754 <79115865+Mai754@users.noreply.github.com>
2025-08-17 16:25:54 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,33 @@
-// firebase-config.js
+import invitados from "./links_invitados.json" assert { type: "json" };
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_DOMINIO.firebaseapp.com",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_BUCKET.appspot.com",
-  messagingSenderId: "XXXXXXXXXXXX",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyB-l_xGz7XRSn0i9yveq4bJnpmr64pmOfE",
+  authDomain: "invitados-1c12c.firebaseapp.com",
+  projectId: "invitados-1c12c",
+  storageBucket: "invitados-1c12c.firebasestorage.app",
+  messagingSenderId: "1026480334740",
+  appId: "1:1026480334740:web:1a40033ab58e696d475181",
+  measurementId: "G-G405RZXXNQ"
 };
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+async function subirInvitados() {
+  for (let invitado of invitados) {
+    await addDoc(collection(db, "invitados"), invitado);
+  }
+}
+
+subirInvitados();
+
+
